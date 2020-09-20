aa5d526d26aff39ef310cb8c9d304db62195a4fa
lizaseledtsova <45207440+lizaseledtsova@users.noreply.github.com>
2020-09-20 23:04:39 +0300

    Update StatRepository.java

--- src/main/java/com/tasklist/backendspringboot/repo/StatRepository.java
+++ src/main/java/com/tasklist/backendspringboot/repo/StatRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.tasklist.backendspringboot.entity.Stat;
 
-// принцип ООП: абстракция-реализация - здесь описываем все доступные способы доступа к данным
+
 @Repository
 public interface StatRepository extends CrudRepository<Stat, Long> {
 
